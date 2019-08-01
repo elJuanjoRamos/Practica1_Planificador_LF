bc9c01536d1f0b5f0010a52b6562d925bdafb4f0
elJuanjoRamos <jjramca1010@gmail.com>
2019-08-01 11:19:23 -0600

    Carga de archivos y lectura en textArea

--- Practica1_Planificador_LF/Form1.Designer.cs
+++ Practica1_Planificador_LF/Form1.Designer.cs
@@ -28,21 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calendario = new System.Windows.Forms.MonthCalendar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Archivo = new System.Windows.Forms.ToolStripDropDownButton();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rutaArchivo = new System.Windows.Forms.TextBox();
+            this.textAnalizar = new System.Windows.Forms.RichTextBox();
+            this.Analizar = new MaterialSkin.Controls.MaterialFlatButton();
+            this.Result = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // calendario
+            // 
+            this.calendario.Location = new System.Drawing.Point(693, 219);
+            this.calendario.Name = "calendario";
+            this.calendario.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Archivo});
+            this.toolStrip1.Location = new System.Drawing.Point(9, 9);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.toolStrip1.Size = new System.Drawing.Size(86, 27);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Archivo
+            // 
+            this.Archivo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Archivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.Archivo.Image = ((System.Drawing.Image)(resources.GetObject("Archivo.Image")));
+            this.Archivo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Archivo.Name = "Archivo";
+            this.Archivo.Size = new System.Drawing.Size(73, 24);
+            this.Archivo.Text = "Archivo";
+            this.Archivo.ToolTipText = "Archivo";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newToolStripMenuItem.Text = "Nueva Pestaña";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Abrir Archivo";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Guardar Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // rutaArchivo
+            // 
+            this.rutaArchivo.Location = new System.Drawing.Point(59, 146);
+            this.rutaArchivo.Name = "rutaArchivo";
+            this.rutaArchivo.Size = new System.Drawing.Size(552, 22);
+            this.rutaArchivo.TabIndex = 6;
+            // 
+            // textAnalizar
+            // 
+            this.textAnalizar.Location = new System.Drawing.Point(59, 178);
+            this.textAnalizar.Name = "textAnalizar";
+            this.textAnalizar.Size = new System.Drawing.Size(552, 617);
+            this.textAnalizar.TabIndex = 7;
+            this.textAnalizar.Text = "";
+            // 
+            // Analizar
+            // 
+            this.Analizar.AutoSize = true;
+            this.Analizar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Analizar.Depth = 0;
+            this.Analizar.Icon = null;
+            this.Analizar.Location = new System.Drawing.Point(749, 146);
+            this.Analizar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Analizar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Analizar.Name = "Analizar";
+            this.Analizar.Primary = false;
+            this.Analizar.Size = new System.Drawing.Size(104, 36);
+            this.Analizar.TabIndex = 8;
+            this.Analizar.Text = "Analizar";
+            this.Analizar.UseVisualStyleBackColor = true;
+            this.Analizar.Click += new System.EventHandler(this.Analizar_Click);
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(722, 498);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(598, 243);
+            this.Result.TabIndex = 9;
+            this.Result.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1326, 746);
+            this.ClientSize = new System.Drawing.Size(1438, 851);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Analizar);
+            this.Controls.Add(this.textAnalizar);
+            this.Controls.Add(this.rutaArchivo);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.calendario);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.MonthCalendar calendario;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TextBox rutaArchivo;
+        private System.Windows.Forms.RichTextBox textAnalizar;
+        private MaterialSkin.Controls.MaterialFlatButton Analizar;
+        private System.Windows.Forms.ToolStripDropDownButton Archivo;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox Result;
     }
 }
 
--- Practica1_Planificador_LF/Form1.cs
+++ Practica1_Planificador_LF/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
+using System.IO;
 
 //Material Design
 using MaterialSkin;
@@ -16,26 +17,113 @@ using MaterialSkin.Controls;
 
 namespace Practica1_Planificador_LF
 {
-    public partial class Form1 : MaterialForm
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-
-            //Inicializa material design
-            MaterialSkinManager ms = MaterialSkinManager.Instance;
-            ms.AddFormToManage(this);
-            ms.Theme = MaterialSkinManager.Themes.LIGHT;
-            /*ms.ColorScheme = new ColorScheme(
-                Primary.Blue400, Primary.Blue500,
-                Primary.Blue500, Accent.LightBlue200,
-                TextShade.WHITE
-            );*/
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        
+
+        private void Analizar_Click(object sender, EventArgs e)
+        {
+
+            /*calendario.MaxSelectionCount = 1;
+            textBox1.Text = calendario.SelectionRange.Start.ToShortDateString();
+            TreeNode newNode = new TreeNode("Text for new node");
+            arbol.SelectedNode.Nodes.Add(newNode);*/
+        }
+
+
+        /*MENU BAR*/
+
+        /*Nueva pestaña*/
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*Abrir archivo*/
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog buscar = new OpenFileDialog();
+
+            if (buscar.ShowDialog() == DialogResult.OK)
+            {
+                rutaArchivo.Text = buscar.FileName;
+
+            }
+
+            if (File.Exists(rutaArchivo.Text))  //verifica si el archivo existe
+            {
+
+                StreamReader leer = new StreamReader(rutaArchivo.Text);
+                
+                string linea;
+                
+                try
+                    {
+                    linea = leer.ReadLine();
+                    while (linea != null)
+                    {
+                        textAnalizar.AppendText(linea + "\n");
+                            linea = leer.ReadLine();
+                        }
+                    analizarCadena();
+
+                }
+
+
+                catch (Exception)
+                    {
+
+                        MessageBox.Show("Error");
+                    }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("No se ha encontrado la ruta o archivo", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+        }
+
+
+        public async void analizarCadena()
+        {
+
+            char[] charsRead = new char[textAnalizar.Text.Length];
+            using (StringReader reader = new StringReader(textAnalizar.Text))
+            {
+                await reader.ReadAsync(charsRead, 0, textAnalizar.Text.Length);
+            }
+
+            StringBuilder reformattedText = new StringBuilder();
+            using (StringWriter writer = new StringWriter(reformattedText))
+            {
+                foreach (char c in charsRead)
+                {
+                    if (char.IsLetter(c) || char.IsWhiteSpace(c) || char.IsDigit(c) || char.IsSeparator(c) || char.IsSymbol(c)
+                        || char.IsLetterOrDigit(c) || char.IsPunctuation(c) || char.IsControl(c) || char.IsSurrogate(c) || char.IsUpper(c))
+                    {
+                        await writer.WriteLineAsync(char.ToLower(c));
+                    }
+                }
+            }
+            Result.Text = reformattedText.ToString();
+
+        }
+
     }
 }
